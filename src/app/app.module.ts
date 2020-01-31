be5e0db7ac267ec91b14087c8f62d6ad63077835
lindsayw89 <lindsay.89@outlook.com>
2020-01-30 18:29:00 -0600

    changes app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BeerComponent } from './beer/beer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import{MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material'
+import { FileDetector } from 'protractor';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +19,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule, 
+     MatTableModule, 
+     MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
