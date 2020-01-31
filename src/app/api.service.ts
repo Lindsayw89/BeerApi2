2228f828170f77a4d0078e539a5128b68d2aba7d
Jordan Woodruff <jordan@neko-chan.local>
2020-01-30 22:58:42 -0600

    api suggestions

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,10 +5,14 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
+// remember to be careful of the default path
 url="https://api.punkapi.com/v2/"
 
   constructor(private httpClient : HttpClient) { }
 async get(path) {
+  // this will call 'https://api/punkapi.com/v2/{path}'
+  // remember my last comment, if you pass a path into this and
+  // do 'url + path' it acts just like a string. that's why you got /beer/beer
   return await this.httpClient.get<any>(this.url + path).toPromise();
 }
 
